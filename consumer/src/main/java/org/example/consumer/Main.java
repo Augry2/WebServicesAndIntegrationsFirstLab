9eba346b06743a8fa6b6ae7b469a8c49b426a79d
august <august.rydnell@gmail.com>
2024-04-17 15:57:45 +0200

    start refactoring from greet to exchange

--- consumer/src/main/java/org/example/consumer/Main.java
+++ consumer/src/main/java/org/example/consumer/Main.java
@@ -2,10 +2,11 @@ package org.example.consumer;
 
 import org.example.service.Greeting;
 
+import java.io.IOException;
 import java.util.ServiceLoader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // creates a list of objects that implement Greeting, load(Greeting.class) tells the ServiceLoader to look for implementations of the Greeting interface
         ServiceLoader<Greeting> loader = ServiceLoader.load(Greeting.class);
 
@@ -13,5 +14,7 @@ public class Main {
             System.out.println(greeting.greet("august"));
         }
 
+        System.out.println("press key to continue...");
+        System.in.read();
     }
 }
