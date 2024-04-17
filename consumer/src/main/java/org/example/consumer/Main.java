7a62008e026d3c18fe39f3e3be3f9214c98f60bc
august <august.rydnell@gmail.com>
2024-04-17 18:09:57 +0200

    finish menu

--- consumer/src/main/java/org/example/consumer/Main.java
+++ consumer/src/main/java/org/example/consumer/Main.java
@@ -14,15 +14,55 @@ public class Main {
         menu(loader);
     }
 
+
+
     public static void menu(ServiceLoader<Exchange> loader) {
-        Scanner fetch = new Scanner(System.in);
         boolean exit = false;
-        String valueSek = enterSek(fetch);
+        while (!exit) {
+            Scanner fetch = new Scanner(System.in);
+            String valueSek = enterSek(fetch);
+            String choice = selectEuroOrDollar(fetch);
 
-
-
+            switch (choice) {
+                case "1":
+                    for (Exchange exchange : loader) {
+                        if (exchange.getCurrency().equals("euro")) {
+                            System.out.println(exchange.exchange(valueSek));
+                        }
+                    }
+                    break;
+                case "2":
+                    for (Exchange exchange : loader) {
+                        if (exchange.getCurrency().equals("dollar")) {
+                            System.out.println(exchange.exchange(valueSek));
+                        }
+                    }
+                    break;
+                case "3":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid choice.");
+            }
+        }
     }
 
+    private static String selectEuroOrDollar(Scanner fetch) {
+        System.out.println("1. Convert to Euro");
+        System.out.println("2. Convert to Dollar");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+        String choice = fetch.nextLine();
+
+        while(true){
+            if (choice.equals("1") || choice.equals("2") || choice.equals("3")) {
+                return choice;
+            }else {
+                System.out.println("Invalid choice. Please enter a valid choice.");
+                choice = fetch.nextLine();
+            }
+        }
+    }
 
 
     public static String enterSek(Scanner fetch){
